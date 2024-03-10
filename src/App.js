53b452a854db7c81d15ec4f76fd4c76437737101
Hino1488 <sexsexboom1@gmail.com>
2024-03-10 12:24:21 +0100

    sigmarchikGif

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
 
     <div className="App">
       <p> Я крутой я сигма сигма крутой я сигма сигма сигма</p>
+
       <header className="App-header">
+        <img src="https://i.gifer.com/6sHG.gif" width="500px" height="500px" />
         <img src={logo} className="App-logo" alt="logo" />
       <img src="https://storage.googleapis.com/pod_public/1300/157553.jpg" width="500px" height="250px"/>
       </header>
