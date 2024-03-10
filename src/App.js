bbdcabb965ccde06b955d890e85c2ca0e6081fe3
Hino1488 <sexsexboom1@gmail.com>
2024-03-10 12:19:46 +0100

    instalReact

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="App">
+      <p> Я крутой я сигма сигма крутой я сигма сигма сигма</p>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      <img src="https://storage.googleapis.com/pod_public/1300/157553.jpg" width="500px" height="250px"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
